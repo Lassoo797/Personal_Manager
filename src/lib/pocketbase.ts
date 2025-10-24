eaa12a56082a3bedabca20078263d5d08dd5e06a
Ladislav Tóth <Ladislav.Toth@outlook.com>
2025-10-24 16:28:28 +0000

    Add env local for setting DB path

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 // Vytvoríme novú inštanciu PocketBase klienta
 // URL adresa musí zodpovedať tej, kde beží vaša PocketBase databáza
-const pb = new PocketBase('http://192.168.50.249:8090');
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
 
 // (Voliteľné) Zapne automatické zrušenie požiadaviek, ak je to potrebné
 pb.autoCancellation(true);
